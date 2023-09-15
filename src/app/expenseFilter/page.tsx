ce309182696f9b88062573292daf86ace2953806
simran019 <simran.19.1677@gmail.com>
2023-09-15 08:21:50 +0530

    applied derived state and computed state concept

--- src/app/expenseFilter/page.tsx
+++ src/app/expenseFilter/page.tsx
@@ -3,7 +3,18 @@
 import { useState } from "react";
 
 const ExpenseFilter = (props:any) => {
-  const [filterYear, setFilterYear] = useState("");
+  const [filterYear, setFilterYear] = useState("2020");
+
+   let filterInfoText='2020, 2021 and 2022';
+   if(filterYear==='2020'){
+    filterInfoText='2021, 2022 and 2023';
+   } else if(filterYear==='2021'){
+    filterInfoText='2020, 2022 and 2023'
+   }else if(filterYear==='2022'){
+    filterInfoText='2020, 2021 and 2023'
+   }else{
+    filterInfoText='2020, 2021 and 2022'
+   }
 
   const clickHandler = (event: any) => {
     const selectedYear = event.target.value;
@@ -23,6 +34,7 @@ const ExpenseFilter = (props:any) => {
         <option value="2022">2022</option>
         <option value="2023">2023</option>
       </select>
+      <p>Data for years {filterInfoText} is hidden</p>
     </div>
   );
 };
